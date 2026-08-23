00b36eedbd28fbbf66af569f56de62b7da93063e
Valentín <valentinlineiro@gmail.com>
2026-08-23 12:59:25 +0200

    feat: add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: 'Valen',
+    description: 'Notas sobre software, sistemas, ideas y vida cotidiana.',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`
+    }))
+  });
+}
